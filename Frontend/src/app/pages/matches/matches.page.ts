cb664e12d1f4c0b616c1daa3b5b272a0c14292a7
Jarosław Leśniak <jarek9244@gmail.com>
2019-08-31 22:49:29 +0200

    Added infinity loading

--- Frontend/src/app/pages/matches/matches.page.ts
+++ Frontend/src/app/pages/matches/matches.page.ts
@@ -11,9 +11,27 @@ export class MatchesPage {
 
   private data;
   private league: League = new League('', '');
+  private matches = [];
+  private index = 0;
 
   constructor(private leagueService: LeagueService) {
     this.data = this.leagueService.getData();
     this.league = this.leagueService.getLeague();
+
+    for(let i = 0; i < 2; i++) {
+      this.matches.push(this.data.match_days[i]);
+    }
+  }
+
+  loadNextMatches(e) {
+    for(let i = 0; i < 2 && this.index < this.data.match_days.length; i++) {
+      this.matches.push(this.data.match_days[this.index]);
+      this.index++;
+    }
+    e.target.complete();
+  }
+
+  componentWillMount() {
+
   }
 }
