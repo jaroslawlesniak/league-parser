578af0c24c433865d8f6529b0ea826d804734601
Jarosław Leśniak <jarek9244@gmail.com>
2019-09-05 19:35:30 +0200

    Fix back button redirect

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, ViewChildren, QueryList } from '@angular/core';
 
-import { Platform, ToastController, ModalController, MenuController, ActionSheetController, AlertController } from '@ionic/angular';
+import { Platform, ToastController, ModalController, MenuController, ActionSheetController, AlertController, IonRouterOutlet } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { Router, RouterOutlet } from '@angular/router';
@@ -11,10 +11,7 @@ import { Router, RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
 
-  lastTimeBackPress = 0;
-  timePeriodToExit = 2000;
-
-  @ViewChildren(RouterOutlet) routerOutlets: QueryList<RouterOutlet>;
+  @ViewChildren(IonRouterOutlet) routerOutlets: QueryList<IonRouterOutlet>;
 
   constructor(private platform: Platform,
     private splashScreen: SplashScreen,
@@ -46,7 +43,6 @@ export class AppComponent {
     } catch (error) {
     }
 
-    // close alert
     try {
         const element = await this.alertCtrl.getTop();
         if (element) {
@@ -56,7 +52,6 @@ export class AppComponent {
     } catch (error) {
     }
 
-    // close modal
     try {
         const element = await this.modalCtrl.getTop();
         if (element) {
@@ -64,23 +59,14 @@ export class AppComponent {
             return;
         }
     } catch (error) {
-        console.log(error);
 
     }
 
-    this.routerOutlets.forEach(async (outlet: RouterOutlet) => {
-          if (this.router.url === '/') {
-            if (new Date().getTime() - this.lastTimeBackPress < this.timePeriodToExit) {
-                navigator['app'].exitApp();
-
-            } else {
-                const toastMsg = await this.toast.create({
-                  message: "Wciśnij jeszcze raz, żeby zamknąć",
-                  duration: 2000
-                });
-                toastMsg.present();
-                this.lastTimeBackPress = new Date().getTime();
-            }
+    this.routerOutlets.forEach(async (outlet: IonRouterOutlet) => {
+        if (this.router.url === '/') {
+            navigator['app'].exitApp();
+        } else if (this.router.url === '/league/table' || this.router.url === '/league/matches') {
+          this.router.navigateByUrl('/');
         }
     });
     }, false);
